3662bc1467fb55d64804ddae39ea1988fc2a8ac9
TakutoMori <mr04vv@gmail.com>
2019-12-04 21:59:17 +0900

    fix: button hover animation

--- src/components/CommonButton/style.ts
+++ src/components/CommonButton/style.ts
@@ -1,38 +1,61 @@
 import styled, { css } from 'styled-components';
 import { Button } from '@material-ui/core';
-import {
-  HOVER_GREEN_COLOR,
-  FONT_COLOR_WHITE,
-  GREEN_COLOR,
-  BLUE_COLOR,
-  HOVER_BLUE_COLOR,
-  HOVER_ORANGE_COLOR,
-  ORANGE_COLOR
-} from '../../styles/color';
+import { FONT_COLOR_WHITE, GREEN_COLOR, BLUE_COLOR, ORANGE_COLOR } from '../../styles/color';
 
 const ButtonStyle = css`
   width: 100%;
   border-radius: 30px;
   text-transform: unset;
+  background: #fff;
+  color: #fff;
+  position: relative;
+
+  ::after {
+    -webkit-transition: all 0.2s;
+    -moz-transition: all 0.2s;
+    -o-transition: all 0.2s;
+    transition: all 0.2s;
+    border-radius: 30px;
+    content: '';
+    position: absolute;
+    z-index: -1;
+    height: 100%;
+    left: 0;
+    top: 0;
+    width: 100%;
+  }
+  :hover:after {
+    height: 0;
+    left: 50%;
+    top: 50%;
+    width: 0;
+  }
 `;
 
 export const CustomGreenButton: React.FC = styled(Button)`
   ${ButtonStyle}
   background-color: ${GREEN_COLOR};
   color: ${FONT_COLOR_WHITE};
+  border: ${GREEN_COLOR} solid 2px;
+  ::after {
+    background-color: ${GREEN_COLOR};
+  }
   :hover {
-    transition: background-color 0s;
-    background-color: ${HOVER_GREEN_COLOR};
+    color: ${GREEN_COLOR};
+    background: rgba(255,255,255,0.08);
   }
 `;
 
 export const CustomBlueButton: React.FC = styled(Button)`
   ${ButtonStyle}
-  background-color: ${BLUE_COLOR};
-  color: ${FONT_COLOR_WHITE};
+  background: ${BLUE_COLOR};
+  border: ${BLUE_COLOR} solid 2px;
   :hover {
-    transition: background-color 0s;
-    background-color: ${HOVER_BLUE_COLOR};
+    color: ${BLUE_COLOR};
+    background: rgba(255,255,255,0.08);
+  }
+  ::after {
+    background-color: ${BLUE_COLOR};
   }
 `;
 
@@ -40,8 +63,12 @@ export const CustomOrangeButton: React.FC = styled(Button)`
   ${ButtonStyle}
   background-color: ${ORANGE_COLOR};
   color: ${FONT_COLOR_WHITE};
+  border: ${ORANGE_COLOR} solid 2px;
+  ::after {
+    background-color: ${ORANGE_COLOR};
+  }
   :hover {
-    transition: background-color 0s;
-    background-color: ${HOVER_ORANGE_COLOR};
+    color: ${ORANGE_COLOR};
+    background: rgba(255,255,255,0.08);
   }
 `;
